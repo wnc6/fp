6b38372aa1fe2cce4c2cd40178c6d7af33b5293f
wnc6 <wchew013@gmail.com>
2024-03-10 23:15:31 +0000

    create data_loader to add MIMIC dataset and OpenI dataset

---
+++ data_loader.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import warnings
+import gensim
+from PIL import Image
+from torch.utils.data import Dataset
+from xray_utils import get_from_csv, read_corpus, report2vec
+
+Image.MAX_IMAGE_PIXELS = None
+warnings.simplefilter('ignore', Image.DecompressionBombWarning)
+
+class MIMIC(Dataset):
+    """
+    A PyTorch Dataset for MIMIC dataset.
+
+    Args:
+        split (str): "train", "test", or "split" indicating the dataset split.
+        data_path (str): Path to the CSV file containing image paths and annotations.
+        transform: PyTorch transforms for image transformations and tensor conversion.
+        annotate: Function to fill in np.nan as in the original chexpert paper.
+    """
+    def __init__(self, split, data_path, transform, annotate):
+        data = pd.read_csv(data_path)
+        self.entries = data[data.split == split]
+        self.transform = transform
+        self.annotate = annotate
+
+    def __getitem__(self, index):
+        img_path = self.entries.iloc[index, 1]
+        img_tensor = self.transform(Image.open(img_path).convert('RGB'))
+        annotation = self.entries.iloc[index, 5:].values
+        label = self.annotate(annotation)
+        return img_tensor, label
+
+    def __len__(self):
+        return len(self.entries)
+
+class OpenI(Dataset):
+    """
+    A PyTorch Dataset for OpenI dataset.
+
+    Args:
+        split (str): "train", "test", or "split" indicating the dataset split.
+        entries_path (str): Path to the CSV file containing image paths and text reports.
+        transform: PyTorch transforms for image transformations and tensor conversion.
+        doc2vec_file (str, optional): Path to the doc2vec model file. Required for non-train splits.
+    """
+    def __init__(self, split, entries_path, transform, doc2vec_file=None):
+        if not (doc2vec_file or split == "train"):
+            raise ValueError("doc2vec must be provided for non-train splits")
+        self.transform = transform
+        self.entries = get_from_csv(entries_path, split)
+        reports = self.entries.iloc[:, 2]
+        self.doc2vec = report2vec(reports, doc2vec_file) if doc2vec_file else report2vec(reports)
+
+    def __getitem__(self, index):
+        img_path = self.entries.iloc[index, 1]
+        img_tensor = self.transform(Image.open(img_path).convert('RGB'))
+        report = self.entries.iloc[index, 2]
+        report_tokens = gensim.utils.simple_preprocess(report)
+        return img_tensor, self.doc2vec.infer_vector(report_tokens)
+
+    def __len__(self):
+        return len(self.entries)
